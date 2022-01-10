6c38dd965a37aa2ab7d838bc634c395cef22851b
asemin08 <semin.allan@gmail.com>
2022-01-10 12:18:40 +0100

    Ajout class launcher

---
+++ src/eu/ensup/gestionglasses/launcher/launcher.java
@@ -0,0 +1,23 @@
+package eu.ensup.gestionglasses.launcher;
+
+
+import eu.ensup.gestionglasses.domaine.Glasse;
+import eu.ensup.gestionglasses.service.GlasseService;
+
+class Launcher {
+
+    public static void main(String[] args) {
+        Glasse glasse1 = new Glasse(1, 123456789, "Rouge", 120.00);
+
+        GlasseService glasseService = new GlasseService();
+        glasseService.creerGlasse(glasse1);
+
+        Glasse glasse2 = glasseService.recuperationGlasse(2);
+        if (glasse2 != null) {
+            System.out.println("LAUNCHER: Récuperation du compte réussie " + glasse2.toString());
+        } else {
+            System.out.println("LAUNCHER: Récuperation des lunettes échoué");
+        }
+    }
+
+}
